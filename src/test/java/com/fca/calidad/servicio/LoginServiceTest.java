43a1e6c63f946139548a8d811eaa45abb21e9575
fca <fca@FCA-Contpaqi-10.inet.uady.mx>
2024-10-30 19:30:20 -0600

    Subiendo loginService

---
+++ src/test/java/com/fca/calidad/servicio/LoginServiceTest.java
@@ -0,0 +1,30 @@
+package com.fca.calidad.servicio;
+import static org.mockito.Mockito.mock;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Test;
+import com.fca.calidad.dao.IDAOUser;
+import com.fca.calidad.modelo.User;
+
+class LoginServiceTest {
+		LoginService service;
+		IDAOUser dao;
+	
+	@Test
+	void logintest() {
+		/*Inicialización*/
+		dao = mock(IDAOUser.class);
+		service =  new LoginService (dao);
+		User usuario = new User("nombre1", "email@email.com", "123456");
+		when(dao.findByUserName("nombre1")).thenReturn(usuario);
+		
+		/*Ejercicio*/
+		boolean result = service.login("nombre1", "123456");
+		
+		/*Resultado*/
+		assertThat(result, is(true));
+		
+	}
+	
+}
