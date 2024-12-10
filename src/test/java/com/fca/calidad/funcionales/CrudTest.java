ecb279ce8abe9c0a722530044d1e14f8274bdf93
12anabell <94195910+12anabell@users.noreply.github.com>
2024-12-09 21:45:07 -0600

    CrudTest.java

--- src/test/java/com/fca/calidad/funcionales/CrudTest.java
+++ src/test/java/com/fca/calidad/funcionales/CrudTest.java
@@ -78,43 +78,48 @@ public class CrudTest {
         driver.findElement(By.xpath("//button[contains(text(),'Yes')]")).click();
     }
 
-    @After
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
+}+
+@After
+public void tearDown() throws Exception {
+  driver.quit();
+  String verificationErrorString = verificationErrorString.toString();
+  if (!"".equals(verificationErrorString)) {
+    fail(verificationErrorString);
   }
 }
+
+private boolean isElementPresent(By by) {
+  try {
+    driver.findElement(by);
+    return true;
+  } catch (NoSuchElementException e) {
+    return false;
+  }
+}
+
+private boolean isAlertPresent() {
+  try {
+    driver.switchTo().alert();
+    return true;
+  } catch (NoAlertPresentException e) {
+    return false;
+  }
+}
+
+private String closeAlertAndGetItsText() {
+  try {
+    Alert alert = driver.switchTo().alert();
+    String alertText = alert.getText();
+    if (acceptNextAlert) {
+      alert.accept();
+    } else {
+      alert.dismiss();
+    }
+    return alertText;
+  } finally {
+    acceptNextAlert = true;
+  }
+}
+}
+   
+   
