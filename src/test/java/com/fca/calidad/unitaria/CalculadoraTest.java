bac566dc287f811a6b0929809e85b5b9a0e65e2c
belen <belen@DESKTOP-9P3UVAD>
2024-12-10 22:35:22 -0600

    Subiendo archivo

--- src/test/java/com/fca/calidad/unitaria/CalculadoraTest.java
+++ src/test/java/com/fca/calidad/unitaria/CalculadoraTest.java
@@ -1,68 +1,67 @@
 package com.fca.calidad.unitaria;
-
+ 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
+ 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import com.fca.calidad.unitaria.Calculadora;
-
+ 
 class CalculadoraTest {
-	private double num1 = 2;
-	private double num2 = 5;
+	
+	private double num1= 10;
+	private double num2= 2;
 	private Calculadora calculadora = null;
+	
 	@BeforeEach
 	void setup() {
-		double num1 = 2;
-		double num2 = 5;
+		double num1= 10;
+		double num2= 2;
 		calculadora = new Calculadora();
-	}
-	@Test
-	void suma2numerosPositivosTest() {
-	/*	double num1 = 2;
-		double num2 = 5;*/
-		double resEsperado = 7;
-		Calculadora calculadora = new Calculadora();
 		
+	}
+ 
+	@Test
+	void suma2numerosPsositivosTest () {
+		double resEsperado = 12;
+		Calculadora calculadora = new Calculadora();
 		double resEjecucion = calculadora.suma(num1, num2);
-		
 		assertThat(resEsperado, is(resEjecucion));
+	
 	}
 	@Test
-	void resta2numerosTest() {
-	/*	double num1 = 2;
-		double num2 = 5;*/
-		double resEsperado = -3;
+	void restar2numerosPsositivosTest () {
+		double resEsperado = 8;
 		Calculadora calculadora = new Calculadora();
-		
 		double resEjecucion = calculadora.resta(num1, num2);
-		
 		assertThat(resEsperado, is(resEjecucion));
+	
 	}
+	
 	@Test
-    void multiplicacion2numerosTest() {
-        double resEsperado = 10; // Corrige el valor esperado
-        double resEjecucion = calculadora.multiplica(num1, num2);
-
-        assertThat(resEjecucion, is(resEsperado));
-    }
-	@Test
-	void division2numerosTest() {
-	/*	double num1 = 10;
-		double num2 = 5;*/
-		double resEsperado = 0.4;
+	void multiplicar2numerosPsositivosTest () {
+		double resEsperado = 20;
 		Calculadora calculadora = new Calculadora();
-		
-		double resEjecucion = calculadora.divide(num1, num2);
-		
+		double resEjecucion = calculadora.multiplica(num1, num2);
+
 		assertThat(resEsperado, is(resEjecucion));
+	
+	}
+	
+	@Test
+	void dividir2numerosPsositivosTest () {
+		double resEsperado = 5;
+		Calculadora calculadora = new Calculadora();
+
+		double resEjecucion = calculadora.divide(num1, num2);
+
+		assertThat(resEsperado, is(resEjecucion));
+	
 	}
 	
 	@AfterEach
 	void print() {
-		System.out.println("Esto se imprime despues de cada prueba");
+		System.out.println("esto se imprime desp de cada prueba");
 	}
-
+ 
 }
